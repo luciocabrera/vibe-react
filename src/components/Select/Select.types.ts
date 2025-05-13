9739fdfa7117e62f7efa58a40d35ae46931dc0f7
Lucio Cabrera <luciocabrera1978@gmail.com>
2025-05-13 07:17:01 +0000

    fix: correct formatting in TSelectProps type definition for consistency

--- src/components/Select/Select.types.ts
+++ src/components/Select/Select.types.ts
@@ -3,6 +3,6 @@ import type { ComponentPropsWithRef } from 'react';
 import type { TFormFieldBase } from '../FormFieldBase/FormFieldBase.types';
 
 export type TSelectProps = ComponentPropsWithRef<'select'> &
-  Pick<TFormFieldBase ,'label' | 'options'> & {
+  Pick<TFormFieldBase, 'label' | 'options'> & {
     showDefaultOption?: boolean;
   };
